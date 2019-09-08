0fcce13dfe3d50f0a0e338e769da31b46c31bf01
Henrique Guimarães <lmarso3@gmail.com>
2019-09-08 18:33:41 +0100

    Process Management

--- RandomVal/Program.cs
+++ RandomVal/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Office.Interop.Excel;
 
 namespace RandomVal
 {
@@ -10,6 +11,24 @@ namespace RandomVal
     {
         static void Main(string[] args)
         {
+            int op = 1;
+
+            do
+            {
+                string _process = Console.ReadLine();
+
+                try
+                {
+                    lib.StartProcess(_process);
+                }
+                catch (Exception)
+                {
+                    lib.StartExcelProcess(_process);
+                }
+
+                Console.ReadKey();
+
+            } while (op != 0);
         }
     }
 }
---
+++ RandomVal/lib.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace RandomVal
+{
+    class lib
+    {
+        public static void StartProcess(string process)
+        {
+            Process.Start(process);
+        }
+
+        public static void StartExcelProcess(string excelprocess)
+        {
+            System.Diagnostics.Process.Start(@"c:\Users\Guima\Desktop\" + excelprocess + ".ods");
+
+        }
+    }
+}
